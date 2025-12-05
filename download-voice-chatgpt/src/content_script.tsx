5108c051b30e8fe773447ce500324ca9030d6fd7
Phuc Le <lecaobaophuc@gmail.com>
2025-12-05 08:53:31 +0700

    Refactor access token extraction logic in content script

--- download-voice-chatgpt/src/content_script.tsx
+++ download-voice-chatgpt/src/content_script.tsx
@@ -14,18 +14,22 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       for (const script of scripts) {
         const text = script.textContent || "";
         if (text.includes("accessToken")) {
-          // Tìm vị trí xuất hiện đầu tiên của chuỗi \"accessToken\",
-          const tokenPart = text.split('\\"accessToken\\",');
+          const tokenPart = text.split(`"accessToken":`);
           if (tokenPart.length > 1) {
             // Lấy phần sau chuỗi đó, rồi tách tiếp theo \\" để lấy giá trị token
-            const afterKey = tokenPart[1];
-            const tokenSplit = afterKey.split('\\"');
-            if (tokenSplit.length > 1) {
-              foundToken = tokenSplit[1];
+            let afterKey = tokenPart[1];
+            // Xóa ký tự " đầu tiên của afterKey rồi gán lại chính nó
+            if (afterKey.startsWith('"')) {
+              afterKey = afterKey.substring(1);
             }
+
+            afterKey = afterKey.split(`",`)[0];
+
+            foundToken = afterKey;
           }
         }
       }
+      console.log("foundToken", foundToken);
       sendResponse({ accessToken: foundToken || null });
     } catch (e) {
       sendResponse({ accessToken: null });
